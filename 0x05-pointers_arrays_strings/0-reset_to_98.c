6aa8af670030c10e8250cbd5e93597172d91c61b
SilimHussein <111010098+SilimHussein@users.noreply.github.com>
2022-09-19 16:35:12 +0000

    pointer resets the value of a parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - uses a pointer to reset its parameter to 98
+ * @n: reset integer
+ *
+ * Return: no return
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains assignments for pointers and arrays
